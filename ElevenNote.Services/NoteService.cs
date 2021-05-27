f08036d4e84b3179ab9b6418f2f12e8a12c7b435
Chris Kronberg <ckronber@gmail.com>
2021-05-27 14:56:35 -0400

    Added Get By Id to NoteService

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -11,6 +11,7 @@ namespace ElevenNote.Services
     public class NoteService
     {
         private readonly Guid _userId;
+
         public NoteService(Guid userId)
         {
             _userId = userId;
@@ -51,6 +52,26 @@ namespace ElevenNote.Services
                             }
                         );
                 return query.ToArray();
+            }
+        }
+
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Notes
+                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                    new NoteDetail
+                    {
+                        NoteId = entity.NoteId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
             }
         }
     }
--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -21,27 +21,7 @@ namespace ElevenNote.WebAPI.Controllers
             return Ok(note);
         }
 
-        public IHttpActionResult GetNoteById(int id)
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var entity =
-                    ctx
-                    .Notes
-                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
-                return
-                    new NoteDetail
-                    {
-                        NoteId = entity.NoteId,
-                        Title = entity.Title,
-                        Content = entity.Content,
-                        CreatedUtc = entity.CreatedUtc,
-                        ModifiedUtc = entity.ModifiedUtc
-                    };
-            }
-        }
-
-
+        
         public IHttpActionResult Post(NoteCreate note)
         {
             if (!ModelState.IsValid)
